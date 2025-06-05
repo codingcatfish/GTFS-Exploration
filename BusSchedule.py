b4e6d035779c5d35543d7b4b052bea290cc89a3d
Angelina Li <untoldnovella@gmail.com>
2025-06-04 20:26:39 -0400

    Update BusSchedule.py
    Added histograms for visualizing the distribution of bus travel lengths and trip distances. Updated comment headers to accurately describe functionality.

--- BusSchedule.py
+++ BusSchedule.py
@@ -1,5 +1,8 @@
-#Builds a basic schedule showing which trips stop where and when.
+# Builds a basic schedule showing which trips stop where and when.
+# Also calculates bus statistics such as total distance traveled in a day, time spent traveling, and total stops.
+# Saveable to a CSV and histograms.
 import pandas as pd
+import matplotlib.pyplot as plt
 import TripDistance as distCalc
 from pathlib import Path
 
@@ -15,7 +18,6 @@ schedule = read("trips.txt")[["trip_id", "block_id", "shape_id"]]
 
 stop_times = read("stop_times.txt")[["trip_id", "stop_sequence", "arrival_time", "departure_time"]]
 stop_times = stop_times.sort_values(by=["trip_id", "stop_sequence"]).groupby("trip_id")
-
 
 first_stops = stop_times.first().reset_index()
 last_stops = stop_times.last().reset_index()
@@ -33,9 +35,7 @@ schedule = schedule.drop(columns="shape_id")
 
 schedule = schedule.sort_values(by=["block_id", "arrival_time"])
 
-
 #Bus statistics
-
 schedule["arrival_time"] = pd.to_timedelta(schedule["arrival_time"])
 schedule["departure_time"] = pd.to_timedelta(schedule["departure_time"])
 
@@ -57,10 +57,28 @@ bus_stops = schedule["trip_id"].apply(numStops).reset_index()
 bus_stops.rename(columns = {"trip_id":"total stops"}, inplace = True)
 
 bus_info = bus_distances.merge(bus_times[["Travel time (in hours)","block_id"]], on = "block_id").merge(bus_stops, on = "block_id")
+print("Total buses: ", len(bus_info))
 print(bus_info)
+
+# Trip distance histogram
+plt.subplot(1,2,1)
+c, b, bars = plt.hist(bus_info["Trip distance (in miles)"], bins = 20)
+plt.bar_label(bars)
+plt.xlabel("Trip distance (in miles)", fontsize = 15)
+plt.ylabel("Number of buses", fontsize = 15)
+plt.title("Distribution of Trip Distances (Miles)", fontsize = 20)
+
+# Travel time histogram
+plt.subplot(1,2,2)
+c, b, bars = plt.hist(bus_times["Travel time (in hours)"].dt.total_seconds()/3600, bins = 20)
+plt.bar_label(bars)
+plt.xlabel("Travel time (in hours)", fontsize = 15)
+plt.ylabel("Number of buses", fontsize = 15)
+plt.title("Distribution of Travel Lengths (Hours)", fontsize = 20)
+plt.show()
 
 
 #for key, item in schedule:
-   # print(schedule.get_group(key),"\n\n")
-#schedule.to_csv(data_path / "bus_schedules.csv", index=False)
-#bus_info.to_csv(data_path / "bus_stats.csv", index=False)
+#    print(schedule.get_group(key),"\n\n")
+#schedule.to_csv(data_path / "bus_schedules.csv", index=False)  #Bus schedule by group
+#bus_info.to_csv(data_path / "bus_stats.csv", index=False)      #Bus statistics
